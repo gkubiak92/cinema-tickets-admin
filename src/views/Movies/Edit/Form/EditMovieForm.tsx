429a7197d42a697f3fab6737977de0e8d50df86f
gkubiak92 <gkubiak92@gmail.com>
2020-06-23 22:05:42 +0200

    Some fixes, but TODO: improve very bad performance

--- src/views/Movies/Edit/Form/EditMovieForm.tsx
+++ src/views/Movies/Edit/Form/EditMovieForm.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch } from "react";
-import { Formik, FieldArray, Form, Field } from "formik";
+import { Formik, FieldArray, Form, Field as FastField } from "formik";
 import {
   IEditMovieFormProps,
   IEditMovieFormMappedDispatch,
@@ -33,6 +33,7 @@ const EditMovieForm = ({
   IEditMovieFormMappedDispatch &
   IEditMovieFormMappedState) => {
   const rootClasses = useRootStyles();
+  console.log("rerender of editmovie form");
 
   return (
     <>
@@ -41,117 +42,162 @@ const EditMovieForm = ({
         onSubmit={(values) => {
           addMovie(values);
         }}
+        validateOnChange={false}
       >
-        {(props) => (
-          <Form>
-            <Field as={TextField} label="Id" fullWidth name="id" disabled />
-            <Field as={TextField} label="Title" fullWidth name="title" />
-            <Rating rating={movie.rating} showTitle />
-            <Field as={TextField} label="Director" fullWidth name="director" />
-            <Field
-              as={TextField}
-              label="Description"
-              fullWidth
-              name="description"
-              multiline
-              rows={4}
-            />
-            <FormGroup row>
-              <FormControlLabel
-                label="New"
-                control={
-                  <Field
-                    as={Checkbox}
-                    name="isNew"
-                    checked={props.values.isNew}
-                    onChange={props.handleChange}
+        {(props) => {
+          console.log("Rerender of editmovie form");
+          return (
+            <Form>
+              <Grid container spacing={2}>
+                <Grid item container xs={4}>
+                  <FastField
+                    as={TextField}
+                    label="Id"
+                    fullWidth
+                    name="id"
+                    disabled
                   />
-                }
-              />
-              <FormControlLabel
-                label="Popular"
-                control={
-                  <Field
-                    as={Checkbox}
-                    name="isPopular"
-                    checked={props.values.isPopular}
-                    onChange={props.handleChange}
+                  <FastField
+                    as={TextField}
+                    label="Title"
+                    fullWidth
+                    name="title"
                   />
-                }
-              />
-              <FormControlLabel
-                label="Upcoming"
-                control={
-                  <Field
-                    as={Checkbox}
-                    name="isUpcoming"
-                    checked={props.values.isUpcoming}
-                    onChange={props.handleChange}
+                  <Rating rating={movie.rating} showTitle />
+                  <FastField
+                    as={TextField}
+                    label="Director"
+                    fullWidth
+                    name="director"
                   />
-                }
-              />
-            </FormGroup>
-            <p>Genres</p>
-            <FieldArray
-              name="genres"
-              render={({ push, remove }) => (
-                <Grid container>
-                  {props.values.genres.map((genre, index) => (
-                    <Grid item xs={3} key={`genre${index}`}>
-                      <Field as={TextField} name={`genres.${index}`} />
-                      <IconButton onClick={() => remove(index)}>
-                        <Delete />
-                      </IconButton>
-                    </Grid>
-                  ))}
-                  <IconButton onClick={() => push("")}>
-                    <Add />
-                  </IconButton>
                 </Grid>
-              )}
-            />
-            <p>Photos Url</p>
-            <FieldArray
-              name="photosUrl"
-              render={({ push, remove }) => (
-                <div>
-                  {props.values.photosUrl.map((photoUrl, index) => (
-                    <Grid container key={index}>
-                      <Grid item xs={10}>
-                        <Field
-                          as={TextField}
-                          fullWidth
-                          name={`photosUrl.${index}`}
-                        />
-                      </Grid>
-                      <Grid item xs={2}>
+                <Grid item container xs={8}>
+                  <FastField
+                    as={TextField}
+                    label="Description"
+                    fullWidth
+                    name="description"
+                    multiline
+                    rows={4}
+                  />
+                </Grid>
+              </Grid>
+              <FormGroup row>
+                <FormControlLabel
+                  label="New"
+                  control={
+                    <FastField
+                      as={Checkbox}
+                      name="isNew"
+                      checked={props.values.isNew}
+                      onChange={props.handleChange}
+                    />
+                  }
+                />
+                <FormControlLabel
+                  label="Popular"
+                  control={
+                    <FastField
+                      as={Checkbox}
+                      name="isPopular"
+                      checked={props.values.isPopular}
+                      onChange={props.handleChange}
+                    />
+                  }
+                />
+                <FormControlLabel
+                  label="Upcoming"
+                  control={
+                    <FastField
+                      as={Checkbox}
+                      name="isUpcoming"
+                      checked={props.values.isUpcoming}
+                      onChange={props.handleChange}
+                    />
+                  }
+                />
+              </FormGroup>
+              <p>Genres</p>
+              <FieldArray
+                name="genres"
+                render={({ push, remove }) => (
+                  <Grid container>
+                    {props.values.genres.map((genre, index) => (
+                      <Grid item xs={3} key={`genre${index}`}>
+                        <FastField as={TextField} name={`genres.${index}`} />
                         <IconButton onClick={() => remove(index)}>
                           <Delete />
                         </IconButton>
                       </Grid>
-                    </Grid>
-                  ))}
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    startIcon={<Add />}
-                    onClick={() => push("")}
-                  >
-                    Add photo Url
-                  </Button>
-                </div>
-              )}
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              className={rootClasses.crudButton}
-            >
-              Save
-            </Button>
-          </Form>
-        )}
+                    ))}
+                    <IconButton onClick={() => push("")}>
+                      <Add />
+                    </IconButton>
+                  </Grid>
+                )}
+              />
+              <p>Stars</p>
+              <FieldArray
+                name="stars"
+                render={({ push, remove }) => (
+                  <Grid container>
+                    {props.values.stars.map((star, index) => (
+                      <Grid item xs={3} key={`star${index}`}>
+                        <FastField as={TextField} name={`stars.${index}`} />
+                        <IconButton onClick={() => remove(index)}>
+                          <Delete />
+                        </IconButton>
+                      </Grid>
+                    ))}
+                    <IconButton onClick={() => push("")}>
+                      <Add />
+                    </IconButton>
+                  </Grid>
+                )}
+              />
+              <p>Photos Url</p>
+              <FieldArray
+                name="photosUrl"
+                render={({ push, remove }) => (
+                  <div>
+                    {props.values.photosUrl.map((photoUrl, index) => (
+                      <Grid container key={index}>
+                        <Grid item xs={10}>
+                          <FastField
+                            as={TextField}
+                            fullWidth
+                            name={`photosUrl.${index}`}
+                          />
+                        </Grid>
+                        <Grid item xs={2}>
+                          <IconButton onClick={() => remove(index)}>
+                            <Delete />
+                          </IconButton>
+                        </Grid>
+                      </Grid>
+                    ))}
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      startIcon={<Add />}
+                      onClick={() => push("")}
+                    >
+                      Add photo Url
+                    </Button>
+                  </div>
+                )}
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                className={rootClasses.crudButton}
+              >
+                Save
+              </Button>
+            </Form>
+          );
+        }}
       </Formik>
       <Snackbar open={!!message} autoHideDuration={3000}>
         <p>{message}</p>
